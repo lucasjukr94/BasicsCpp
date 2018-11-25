9fb1708982e4a38cd08031015561c130d844436a
lucasjukr94 <lucasjukr94@gmail.com>
2018-11-25 20:59:18 -0200

    add prob solv

---
+++ Basics/OpenGL/OpenGL.cpp
@@ -0,0 +1,13 @@
+// OpenGL.cpp : Este arquivo contém a função 'main'. A execução do programa começa e termina ali.
+//
+
+#include "pch.h"
+#include <windows.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n"; 
+}
